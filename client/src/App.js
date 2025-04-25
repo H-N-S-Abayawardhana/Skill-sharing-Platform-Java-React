d11458f8c4ff58a9e6f852c4df4c959dfc64b2b3
H-N-S-Abayawardhana <hnsabayawardhana@gmail.com>
2025-04-25 22:51:39 +0530

    Commit before add RUD parts

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import {Route,Routes} from "react-router";
+import { Route, Routes } from "react-router-dom";
+
 import Home from './Pages/Home';
 import AddLearningPlan from './Pages/LearningPlan/AddLearningPlan';
 
 
 
 
-import Home from "./pages/Home";
-
 
 function App() {
   return (
     <div>
-      <React.Fragment>
+      
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/learning-plan" element={<h1>Learning Plan</h1>} />
           <Route path="/learning-plan/add" element={<AddLearningPlan/>} />
           <Route path="/learning-plan/:id" element={<h1>Learning Plan Detail</h1>} />
         </Routes>
-      </React.Fragment>
+      
 
     </div>
   );
